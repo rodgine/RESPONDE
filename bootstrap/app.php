01f92c6950482daf2300cbb63ccf986aee7fc5a8
Rodgine Mallari <153807677+rodgine@users.noreply.github.com>
2025-11-07 16:20:52 +0800

    Update app.php added CorsMiddleware for header request issue

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => RoleMiddleware::class,
         ]);
+        $middleware->append(\App\Http\Middleware\CorsMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
